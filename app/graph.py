134ba451072ec89cd5bb27c80198eb0797ffc95d
Drew-Andersen <andrewtandersen@gmail.com>
2026-02-24 14:33:28 -0500

    Added thread-level persistance

--- app/graph.py
+++ app/graph.py
@@ -5,17 +5,21 @@ from app.state import GraphState
 from app.nodes.retrieve import retrieve
 from app.nodes.generate import generate
 
+# SQLite database for checkpointing
 conn = sqlite3.connect("langgraph_checkpoint.db", check_same_thread=False)
+saver = SqliteSaver(conn=conn)
 
+# Builds the base graph once
 builder = StateGraph(GraphState)
 
 builder.add_node("retrieve", retrieve)
 builder.add_node("generate", generate)
-
 builder.add_edge(START, "retrieve")
 builder.add_edge("retrieve", "generate")
 builder.add_edge("generate", END)
 
-memory = SqliteSaver(conn=conn)
+def get_graph(thread_id: str):
+    conn = sqlite3.connect("langgraph_checkpoint.db", check_same_thread=False)
+    saver = SqliteSaver(conn=conn)
 
-graph = builder.compile(checkpointer=memory)
+    return builder.compile(checkpointer=saver)
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import uuid
 from langchain_core.messages import HumanMessage
-from app.graph import graph
+from app.graph import get_graph
 from app.chroma_client import ingest_pdf
 
 # ingest_pdf("Foundations_of_Strength_Training.pdf")
 
-config = {"configurable": {"thread_id": "user-1"}}
+thread_id = str(uuid.uuid4())
+graph = get_graph(thread_id)
 
 while True:
     question = input("\nAsk: ")
+    if question.lower() in ["exit", "quit"]:
+        break
+
+    config = {"thread_id": thread_id}
 
     result = graph.invoke(
         {"messages": [HumanMessage(content=question)]},
